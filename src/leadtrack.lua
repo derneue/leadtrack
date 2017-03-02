3913e938695ab1c6128a9ce13c0bfd431e2e19cd
derneue <derneue@users.noreply.github.com>
2017-03-02 21:28:40 +0100

    Added creation option for a new journal Added list option for given data

--- src/leadtrack.lua
+++ src/leadtrack.lua
@@ -27,22 +27,83 @@ helptext = [[
   l			List journal of the day
   n			Add new entry of the day
   d			Delete entry
+  e			Exit
 ]]
+
+
+-- Preliminary data tank
+tank = {}
 
 function print_helptext(param)
 	print(helptext)
 end
 
 function cmd_list_journal(param)
-	print("Print journal")
+	print("Journal items for today")
+	print("-----------------------")
+	print("")
+	
+	-- Iterate over data tank
+	for _, v in pairs(tank) do
+		print("   "..v["short_desc"])
+		print("   Dur: "..v["duration"].."  Pay: "..v["paytype"].."  Date: "..v["date"])
+		print("")	
+	end
 end
 
 function cmd_add_new_journal_entry(param)
-	print("New journal entry added")
+	local short_desc
+	local long_desc
+	local duration
+	local paytype
+	
+	print("Add new journal entry")
+	print("---------------------")
+	print("")
+
+	-- Read short description
+	io.write("Short description [none]: ")
+	short_desc = io.read()
+	
+	-- Read duration
+	io.write("Duration (h) [none]: ")
+	duration = io.read()
+
+	-- Read paytype
+	io.write("Project paytype [none]: ")
+	paytype = io.read()
+
+	-- Read long description by launching external
+	-- editor.
+	-- Generate temporary file
+	local tmpfile = os.tmpname()
+	print("Launching external editor for long desc...")
+	os.execute("vim "..tmpfile)
+	
+	local long_desc_fd = io.open(tmpfile, "r")
+	long_desc = long_desc_fd:read("*a")
+	
+	-- Closing file descriptor and remove file
+	long_desc_fd:close()	
+	os.remove(tmpfile)	
+	-- Write data to tank
+	local entry = {}
+	entry["short_desc"] = short_desc
+	entry["long_desc"] = long_desc
+	entry["duration"] = duration
+	entry["paytype"] = paytype
+	entry["date"] = os.date()
+
+	table.insert(tank, entry) 
+
 end
 
 function cmd_delete_journal_entry(param)
 	print("Journal entry deleted")
+end
+
+function cmd_exit(param)
+	ltstate = "DO_EXIT"
 end
 
 -- Fill callback table
@@ -51,6 +112,7 @@ ctable = {
 	{ txt="l", cmd=cmd_list_journal },
 	{ txt="n", cmd=cmd_add_new_journal_entry },
 	{ txt="d", cmd=cmd_delete_journal_entry },
+	{ txt="e", cmd=cmd_exit },
 }
 
 ltstate = "DO_RUN"
